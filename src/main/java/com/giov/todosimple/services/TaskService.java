fc73791554eeacb9268f42c5539465862f771c9f
Giovana Ferreira <giovannapink26@gmail.com>
2025-04-09 20:20:59 -0300

    09/04 - Criação dos services de Task e User

---
+++ src/main/java/com/giov/todosimple/services/TaskService.java
@@ -0,0 +1,49 @@
+package com.giov.todosimple.services;
+
+import com.giov.todosimple.models.Task;
+import com.giov.todosimple.models.User;
+import com.giov.todosimple.repositories.TaskRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id){
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException("Task not found ! \nId: " + id + "\nType:" + Task.class.getSimpleName()));
+    }
+
+    @Transactional
+    public Task create(Task obj){
+        User user = this.userService.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj = this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj){
+        Task newObj = this.findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return taskRepository.save(newObj);
+    }
+
+    public Task delete(Long id){
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        }catch (Exception e){
+            throw new RuntimeException("Error deleting task because already relational entity's! \nId: " + id);
+        }
+        return this.taskRepository.findById(id).get();
+    }
+}
---
+++ src/main/java/com/giov/todosimple/services/UserService.java
@@ -0,0 +1,48 @@
+package com.giov.todosimple.services;
+
+import com.giov.todosimple.models.User;
+import com.giov.todosimple.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service // ANNOTATION RESPONSAVEL POR INDICAR QUE E UM SERVICE (SOBE JUNTO COM O SERVIDOR)
+// USADO PARA SE COMUNICAR COM OS REPOSITORIES )
+public class UserService {
+    @Autowired //
+    private UserRepository userRepository;
+
+    public User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException("User not found ! \nId: " + id +
+                                                            "\nUser: " + user +
+                                                            "\nType:" + User.class.getSimpleName()));
+    }
+
+    @Transactional // ANNOTATION RESPONSAVEL POR CONTROLAR A ALTERACAO/INCLUSAO DE REGISTROS NO BANCO
+    //PARECIDO COM ROLLBACK
+    public User create(User obj){
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public User delete(Long id){
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        }catch (Exception e){
+            throw new RuntimeException("Error deleting user because already relational entity's! \nId: " + id);
+        }
+        return this.userRepository.findById(id).get();
+    }
+}
